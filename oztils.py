521dfaeb9ecef2d8541bc5bf9b67511ca3cefc59
ozzy <businessozzy27@gmail.com>
2022-10-25 20:46:06 -0400

    Added new features
    - isPalindrome function (checks if something is a palindrome, even a list!)
    - listToString (exactly what is sounds like.)

--- oztils.py
+++ oztils.py
@@ -4,9 +4,9 @@ def nonIndexedRange(rangeInp):
 	for i in lst: lst[i]+=1
 	return lst
 
-#If input "Hello", returns "olleH"
-def invertString(strInp):
-	return str(strInp)[::-1]
+#If input "Hello", returns "olleH" - if input 125, return 521. If input ["world!", "Hello,"], return ["Hello,", "world!"]
+def invert(strInp):
+	return strInp[::-1]
 
 #Removes version compatability - removeStart("Hello! ", "Hello! My name is Bill!") returns "My name is Bill!"
 def removeStart(start, strInp):
@@ -21,3 +21,11 @@ def removeEnd(end, strInp):
 		return strInp[:-endLength]
 	else:
 		return strInp	
+#Checks if an input (string, int, list) is a palindrome
+def isPalindrome(check):
+	if check==check[::-1]: return True
+	else: return False
+
+#turns list into string
+def listToString(listInp):
+	return ''.join(listInp)
