ad8334fa293461502d550615b662e911e1e48ce7
ozzy <businessozzy27@gmail.com>
2023-03-16 11:01:31 -0400

    fix

--- Spell Check.py
+++ Spell Check.py
@@ -3,4 +3,5 @@ from english_words import english_words_set as words
 def close(string):
 	try:return get_close_matches(string.lower(),words)[0]
 	except:return "No match found"
-if __name__ == "__main__":while True: print(close(input("> ")))
+if __name__ == "__main__":
+	while True: print(close(input("> ")))
